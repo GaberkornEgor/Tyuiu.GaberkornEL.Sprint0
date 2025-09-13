03641ea4b5e951203ab09e51825d77107272e47b
GaberkornEL <gaber.egors@gmail.com>
2025-09-13 13:12:05 +0500

    Task2

---
+++ Tyuiu.GaberkornEL.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GaberkornEL.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.GaberkornEL.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        public void CheckGetMessageValid()
+        {
+            var name = "Егор";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Егор", res);
+        }
+    }
+}
