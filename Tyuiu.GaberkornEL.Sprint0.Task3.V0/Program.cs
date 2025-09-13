8c8315a9d1dc08520bd06a68e45e0cabfadf0db5
GaberkornEL <gaber.egors@gmail.com>
2025-09-13 13:42:00 +0500

    Task3

---
+++ Tyuiu.GaberkornEL.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.GaberkornEL.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.GaberkornEL.Sprint0.Task3.V0.Lib
+{
+
+    class Progam
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
